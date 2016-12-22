e753cc051b1c59b21c70229d46c075e06e93989a
zhenjiang.dou <zhenjiang.dou@nextcont.com>
2016-12-22 17:00:01 +0800

    代码

--- legal_terms.php
+++ legal_terms.php
@@ -1,5 +1,5 @@
 <?php
-	require_once ("core.php");
+    require_once ("core.php");
     $html = file_get_contents('https://ifastnet.com/portal/terms.php');
     // preg_match('/<footer[^>]*id="footer"[^>]*>(.*?) <//footer>/si', $html, $match);
 ?>
