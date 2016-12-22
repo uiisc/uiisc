b0e60c77326f9139e0754fbf7a4c2f5ebae946ec
zhenjiang.dou <zhenjiang.dou@nextcont.com>
2016-12-22 17:09:49 +0800

    哈哈

--- core.php
+++ core.php
@@ -1,9 +1,9 @@
 <?php
-$title = "UIISC";
-$title_s = "UIISC";
-$author = 'Crogram Inc.';
-$description = "uiisc, freewebhost, webhost, Crogram";
-$rooturl = $_SERVER['HTTP_HOST'];
-$domain = preg_replace('/^www\./', '', $rooturl);
-$static_release = "1423465754";
+    $title = "UIISC";
+    $title_s = "UIISC";
+    $author = 'Crogram Inc.';
+    $description = "uiisc, freewebhost, webhost, Crogram";
+    $rooturl = $_SERVER['HTTP_HOST'];
+    $domain = preg_replace('/^www\./', '', $rooturl);
+    $static_release = "1423465754";
 ?>
