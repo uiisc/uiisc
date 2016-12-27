1d5888c6dbec9b9a4c4f7090693fe1fd5fb4cf5c
zhenjiang.dou <zhenjiang.dou@nextcont.com>
2016-12-27 18:00:29 +0800

    update

--- login.php
+++ login.php
@@ -5,7 +5,7 @@
 <html lang="en">
 <head>
     <meta charset="utf-8">
-    <title><?=$title?>-Login</title>
+    <title><?=$title?> - Login</title>
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0, user-scalable=no">
     <meta name="description" content="<?=$description?>">
