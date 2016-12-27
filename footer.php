1d964a184cc2be1675f7be7a2bf31b3ba81f6c20
zhenjiang.dou <zhenjiang.dou@nextcont.com>
2016-12-27 11:23:15 +0800

    改正链接

--- footer.php
+++ footer.php
@@ -2,14 +2,14 @@
     <div class="container">
         <div class="navbar-inner navbar-content-center" style="padding-top:15px;">
             <ul class="navbar-left list-inline text-left text-muted credit">
-                <li>&copy; 2016 <a href="./index1.php"><?=$title_s?></a> &nbsp;&nbsp;&nbsp; Powered By <a href="https://crogram.com" target="blank">Crogram</a> & <a href="https://ifastnet.com" target="blank">iFastNet</a></li>
+                <li>&copy; 2016 <a href="./index.php"><?=$title_s?></a> &nbsp;&nbsp;&nbsp; Powered By <a href="https://crogram.com" target="blank">Crogram</a> & <a href="https://ifastnet.com" target="blank">iFastNet</a></li>
             </ul>
             <ul class="legal navbar-right list-inline text-center">
-                <li><a href="./index1.php" data-i18n="home">Home</a></li>
+                <li><a href="./index.php" data-i18n="home">Home</a></li>
                 <li><a href="./legal_terms.php" data-i18n="tos">Terms of service</a></li>
-                <li><a href="./contact1.php" data-i18n="contact_us">Contact us</a></li>
+                <li><a href="./contact.php" data-i18n="contact_us">Contact us</a></li>
                 <li><a href="//help.<?=$domain?>" data-i18n="help">Help</a></li>
-                <li><a href="./news1.php" data-i18n="news">News</a></li>
+                <li><a href="./news.php" data-i18n="news">News</a></li>
             </ul>
         </div>
     </div>
