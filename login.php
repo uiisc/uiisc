af3f7b6acf3d72bf49011a367b6a8dde4bb725cb
zhenjiang.dou <zhenjiang.dou@nextcont.com>
2016-12-27 13:24:42 +0800

    修改更新

--- login.php
+++ login.php
@@ -21,14 +21,15 @@
 <body>
 <?php include ("nav.php"); ?>
 <div class="container">
-    <form class="form-signin" role="form" action="//cpanel.<?=$domain?>.com/login.php" method="post" name="login">
+    <form class="form-signin" role="form" action="//cpanel.<?=$domain?>/login.php" method="post" name="login">
         <h2 class="form-signin-heading" data-i18n="please_login">Please sign in</h2>
         <input type="text" name="uname" class="form-control" placeholder="Username" data-i18n="input_username" required autofocus>
         <input type="password" name="passwd" class="form-control" placeholder="Password" data-i18n="input_password" required>
-            <!--<label>-->
-                <input type="checkbox" value="remember-me"><span data-i18n="remember_me"> Remember me</span><a href="https://panel.byethost.com/lostpassword.php">Lost your password?</a>
-            <!--</label>-->
         <div class="checkbox">
+            <label>
+                <input type="checkbox" value="remember-me"><span data-i18n="remember_me"> Remember me</span>
+                <a href="https://panel.byethost.com/lostpassword.php" data-i18n="lost_password">Lost your password?</a>
+            </label>
         </div>
         <button type="submit" name="submit" class="btn btn-lg btn-primary btn-block" data-i18n="login">Sign in</button>
     </form>
