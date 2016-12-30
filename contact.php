c235c14b129b3631c1b7a6a63939d0ea59b7eaff
zhenjiang.dou <zhenjiang.dou@nextcont.com>
2016-12-30 14:58:52 +0800

    add contact.php

--- contact.php
+++ contact.php
@@ -1,55 +1,53 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
+<?php
+	require_once ("core.php");
+?>
+<!DOCTYPE html>
+<html lang="en">
 <head>
-<title><? echo "$yourdomain" ;?>web hosting</title>
-<meta http-equiv="content-type" content="text/html; charset=utf-8" />
-<link href="default.css" rel="stylesheet" type="text/css" />
+    <meta charset="utf-8">
+    <title><?=$title?> - News</title>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0, user-scalable=no">
+    <meta name="description" content="<?=$description?>">
+    <meta name="author" content="<?=$author?>">
+    <link href="favicon.ico?_=<?=$static_release?>" rel="icon">
+    <link href="//cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet">
+    <link href="./css/style.css?_=<?=$static_release?>" rel="stylesheet"><!--[if lt IE 9]>
+    <script src="./js/ie8-responsive-file-warning.js"></script><![endif]-->
+    <script src="./js/ie-emulation-modes-warning.js"></script><!--[if lt IE 9]>
+    <script src="//cdn.bootcss.com/html5shiv/3.7.0/html5shiv.js"></script>
+    <script src="//cdn.bootcss.com/respond.js/1.4.2/respond.min.js"></script><![endif]-->
 </head>
 <body>
-  <? 
-    $yourdomain = $_SERVER['HTTP_HOST'];
-    $yourdomain = preg_replace('/^www\./' , '' , $yourdomain);
-    ?>
-
- <? include ("menu.php"); ?>
-
-<div id="page">
-  <div id="content">
-    <div id="welcome">
-
-  <h2>Contact Us</h2>
-  <p>If you have any problems or have the need to contact us to ask a question,<br />
-      you can use the <span>integrated support system</span> in your control panel to create a support ticket.<br />
-   
-      We will reply to your question as soon as possible.
-<br><br />
-      For technical support please look at the <a href="http://byet.net/forumdisplay.php?f=28">Knowledge Base</a>
-</div>
-  </div>
-  <!-- end #content -->
-  <div id="sidebar">
-    <div id="links">
-      <ul>
-
-    <li class="first"><a href="index.php"><b>H</b>omepage</a></li>
-    <li><a href="signup.php" accesskey="A"><b>S</b>ignup</a></li>
-    <li><a href="news.php" accesskey="P"><b>P</b>roduct new</a></li>
-    <li><a href="contact.php" accesskey="U">Contact <b>U</b>s</a></li>
-    <li><a href="https://ifastnet.com/portal/terms.php" accesskey="S"><b>T</b>erms of service</a></li>
-      </ul>
+<?php include ("nav.php"); ?>
+<div class="container">
+    <div class="row">
+        <section class="section-wrap">
+            <div class="container">
+                <div class="row">
+                    <div class="hidden-xs col-sm-12 col-md-12 termsHead"><h1>Contact Us</h1></div>
+                    <div class="col-sm-12 col-md-12">
+                        <h2>Contact</h2>
+                        <p>If you have any problems or have the need to contact us to ask a question, 
+                        you can use the <span>integrated support system</span> in your control panel to create a support ticket.</p>
+                        <p>We will reply to your question as soon as possible.</p>
+                        <p>For technical support please look at the <a href="http://byet.net/forumdisplay.php?f=28">Knowledge Base</a></p>
+                        <p>This Privacy Policy governs the manner in which IFastNet LTD collects, uses, maintains and
+                            discloses information collected from users of this Web site (each, a "User").</p>
+                    </div>
+                    <div class="col-sm-12 col-md-12">
+                        <h2>Address</h2>
+                        <p>Shanghai China.</p>
+                    </div>
+                    <div class="col-sm-12 col-md-12">
+                        <h2>Others</h2>
+                        <p class="alert alert-warning"><i class="glyphicon glyphicon-info-sign"></i> Crogram Inc.</p>
+                    </div>
+                </div>
+            </div>
+        </section>
     </div>
-    <div>
-      <h2>Instant activation</h2>
-      <blockquote>
-        <p>Free hosting accounts are activated instantly, no need to wait for manual approval, you can start building your pages immediately!  A powerful Vista Panel control panel is provided to manage your website, packed with hundreds of great features including Email, FTP add-on domain and much more..</p>
-      </blockquote>
-    </div>
-  </div>
-  <!-- end #sidebar -->
-  <div style="clear: both; height: 1px;"></div>
 </div>
-<!-- end #page -->
- <? include ("footer.php"); ?>
-
+<?php include ("footer.php"); ?>
 </body>
 </html>
