854f394fc2077955c6b6437f79f89287ec7ae763
唐钰豆豆 <doudoudzj@sina.com>
2017-02-17 02:41:39 +0800

    update year

--- footer.php
+++ footer.php
@@ -2,7 +2,7 @@
     <div class="container">
         <div class="navbar-inner navbar-content-center" style="padding-top:15px;">
             <ul class="navbar-left list-inline text-center text-muted credit">
-                <li>&copy; 2016 <a href="./index.php"><?=$title_s?></a> &nbsp;&nbsp;&nbsp; Powered By <a href="https://crogram.com" target="blank">Crogram</a> & <a href="https://ifastnet.com" target="blank">iFastNet</a></li>
+                <li>&copy; 2017 <a href="./index.php"><?=$title_s?></a> &nbsp;&nbsp;&nbsp; Powered By <a href="https://crogram.com" target="blank">Crogram</a> & <a href="https://ifastnet.com" target="blank">iFastNet</a></li>
             </ul>
             <ul class="legal navbar-right list-inline text-center">
                 <li><a href="./index.php" data-i18n="home">Home</a></li>
