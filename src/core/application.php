663ea7dd4de94180fdb5ac73e9636c0e0e42f37d
Jackson Dou <jksdou@qq.com>
2023-06-01 18:54:19 +0800

    修复回调token不对的问题：区分路径字符大小写

--- src/core/application.php
+++ src/core/application.php
@@ -54,11 +54,11 @@ $site_path = substr($scriptpath, 0, strrpos($scriptpath, '/'));
 $site_url = HTTP_PROTOCOL . '://' . $site_domain;
 
 if (isset($_SERVER['PATH_INFO'])) {
-    $path_info = strtolower(trim(str_replace($_SERVER['SCRIPT_NAME'], '', $_SERVER['PATH_INFO']), '/'));
+    $path_info = trim(str_replace($_SERVER['SCRIPT_NAME'], '', $_SERVER['PATH_INFO']), '/');
 } else if ($_SERVER['REQUEST_URI']) {
-    $request_uri = strtolower(trim($_SERVER['REQUEST_URI']));
+    $request_uri = trim($_SERVER['REQUEST_URI']);
     $path_info = explode('?', $request_uri)[0];
-    $path_info = strtolower(trim(str_replace($site_path, '', $path_info)));
+    $path_info = trim(str_replace($site_path, '', $path_info));
 }
 
 define('SITE_DOMAIN', $site_domain);
